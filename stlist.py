05af46b647fd296367935425367b7e69e9f291c1
Ignacio Torres Masdeu <i@itorres.net>
2014-09-13 00:49:43 +0200

    stlist v0.1

---
+++ stlist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env py
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# Requires the following modules
+#   apt-get install python-requests python-bs4
+#   pip install requests beautifulsoup4 netaddr
+import requests
+import os
+import time
+from bs4 import BeautifulSoup
+zone=2426
+cnml_file="/tmp/%s.cnml" % zone
+cnml_url="http://guifi.net/ca/guifi/cnml/%s/detail" % zone
+
+# http://stackoverflow.com/a/16695277
+def DownloadFile(url, local_filename):
+    print "Downloading %s to %s" % ( url, local_filename)
+    r = requests.get(url)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return 
+try:
+    age=time.time() - os.path.getmtime(cnml_file)
+    print "File age %s" % age
+    if age > 3600*4:
+        print "Too old, redownload"
+        DownloadFile(cnml_url, cnml_file)
+except OSError:
+    print "%s does not exist" % cnml_file
+    DownloadFile(cnml_url, cnml_file)
+
+
+def is_st(tag):
+    if tag.name == "device" and len(tag.find_all("radio")) > 1:
+        print "%6s %30s http://guifi.net/ca/guifi/device/%s" % (tag["id"], tag["title"], tag["id"])
+        return True
+    else:
+        return False
+with open(cnml_file, 'r') as f:
+    cnml = BeautifulSoup(f.read())
+cnml.find_all(is_st)
+#print cnml.find_all("device", {"mode" : "ap"})
