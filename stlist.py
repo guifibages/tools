bb6b35e7eb4d10dcf1cefb67e5fb76e0b44e2cc8
Ignacio Torres Masdeu <i@itorres.net>
2014-09-13 00:57:42 +0200

    Migrate to print function

--- stlist.py
+++ stlist.py
@@ -3,17 +3,24 @@
 # Requires the following modules
 #   apt-get install python-requests python-bs4
 #   pip install requests beautifulsoup4 netaddr
+from __future__ import print_function
+import sys
 import requests
 import os
 import time
 from bs4 import BeautifulSoup
+
 zone=2426
 cnml_file="/tmp/%s.cnml" % zone
 cnml_url="http://guifi.net/ca/guifi/cnml/%s/detail" % zone
 
+# http://stackoverflow.com/a/14981125
+def warning(*objs):
+    print("WARNING: ", *objs, file=sys.stderr)
+
 # http://stackoverflow.com/a/16695277
 def DownloadFile(url, local_filename):
-    print "Downloading %s to %s" % ( url, local_filename)
+    warning( "Downloading %s to %s" % ( url, local_filename) )
     r = requests.get(url)
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
@@ -22,12 +29,12 @@ def DownloadFile(url, local_filename):
     return 
 try:
     age=time.time() - os.path.getmtime(cnml_file)
-    print "File age %s" % age
     if age > 3600*4:
-        print "Too old, redownload"
+        warning( "File age %s" % age )
+        warning( "Too old, redownload")
         DownloadFile(cnml_url, cnml_file)
 except OSError:
-    print "%s does not exist" % cnml_file
+    warning( "%s does not exist" % cnml_file)
     DownloadFile(cnml_url, cnml_file)
 
 
@@ -39,5 +46,4 @@ def is_st(tag):
 with open(cnml_file, 'r') as f:
     cnml = BeautifulSoup(f.read())
 for st in cnml.find_all(is_st):
-    print "%6s %30s http://guifi.net/ca/guifi/device/%s" % (st["id"], st["title"], st["id"])
-#print cnml.find_all("device", {"mode" : "ap"})
+    print("%6s %30s http://guifi.net/ca/guifi/device/%s" % (st["id"], st["title"], st["id"]))
