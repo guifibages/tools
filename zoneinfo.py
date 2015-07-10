c75e00291db817d93ee76cb7dafa8fd96d5d83a8
Ignacio Torres Masdeu <i@itorres.net>
2015-07-10 22:33:21 +0200

    Parameterize cnml_cache and cnml_base url

--- zoneinfo.py
+++ zoneinfo.py
@@ -22,32 +22,27 @@ def DownloadFile(url, local_filename):
 
 
 class ZoneInfo():
-    def __init__(self, zone):
+    def __init__(self, zone, cnml_cache="/tmp",
+                 cnml_base="http://guifi.net/ca/guifi/cnml"):
         self.zone = zone
+        self.cnml_base = cnml_base
+        self.cnml_cache = cnml_cache
         self.zone_id = self.get_zone_id()
-        self.cnml = ZoneInfo.get_zone(self.zone_id)
+        self.cnml = self.get_zone(self.zone_id)
 
     def get_zone_id(self):
         try:
             zone_id = int(self.zone)
         except ValueError:
-            zones = ZoneInfo.get_zone(3671, "zones")
+            zones = self.get_zone(3671, "zones")
             result = sorted(filter(lambda z: self.zone in z.title.lower(),
                                    zones.getZones()), key=lambda z: z.id)
             zone_id = result[0].id
         return zone_id
 
-    def zones(self):
-        for z in self.cnml.getZones():
-            print(z.title, z.id)
-
-    def multi(self):
-        for sn in filter(lambda n: n.totalLinks > 1, self.cnml.getNodes()):
-            print(sn.title, "http://guifi.net/en/node/{0}".format(sn.id))
-
-    def get_zone(zone, kind="detail"):
-        cnml_file = "/tmp/{0}.{1}.cnml".format(zone, kind)
-        cnml_url = "http://guifi.net/ca/guifi/cnml/{0}/{1}".format(zone, kind)
+    def get_zone(self, zone, kind="detail"):
+        cnml_file = "{}/{}.{}.cnml".format(self.cnml_cache, zone, kind)
+        cnml_url = "{}/{}/{}".format(self.cnml_base, zone, kind)
         try:
             age = time.time() - os.path.getmtime(cnml_file)
             if age > 24*3600:
@@ -60,21 +55,29 @@ class ZoneInfo():
         return libcnml.CNMLParser(cnml_file)
 
     def list(self, kind):
-        m = getattr(self, kind)
-        m()
+        getattr(self, kind)()
+
+    def zones(self):
+        for z in self.cnml.getZones():
+            print(z.title, z.id)
+
+    def multi(self):
+        for sn in filter(lambda n: n.totalLinks > 1, self.cnml.getNodes()):
+            print(sn.title, "http://guifi.net/en/node/{0}".format(sn.id))
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Get information from Guifi Zones.')
+    opt_list = parser.add_mutually_exclusive_group(required=True)
     parser.add_argument('zone', nargs="?", default=3671,
                         help="Zone to work with")
-    parser.add_argument('-z', dest='kind', action="store_const",
-                        const='zones', default="zones",
-                        help="List zones")
-    parser.add_argument('-m', dest='kind', action="store_const",
-                        const='multi', default="zones",
-                        help="List ndoes with multiple links")
+    opt_list.add_argument('-z', dest='kind', action="store_const",
+                          const='zones', default="zones",
+                          help="List zones")
+    opt_list.add_argument('-m', dest='kind', action="store_const",
+                          const='multi', default="zones",
+                          help="List nodes with multiple links")
 
     args = parser.parse_args()
     zi = ZoneInfo(args.zone)
